ec7cebc24c574bb61e6c0bb4770bee21a3b57ebd
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-15 23:44:21 +0530

    updated match

--- src/main/java/com/gb/cricscore/model/match/Match.java
+++ src/main/java/com/gb/cricscore/model/match/Match.java
@@ -1,18 +1,23 @@
 package com.gb.cricscore.model.match;
 
+import com.gb.cricscore.model.people.Umpire;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 public class Match {
+    private String matchId;
+    private Tournament tournament;
     private final PlayedTeams playedTeams;
     private String venue;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private MatchType matchType;
+    private List<Umpire> umpires;
+    private String winner;
 
     public Match(PlayedTeams playedTeams) {
         this.playedTeams = playedTeams;
