966d60507762c41cdf40a918ca58aaf0cf9c5309
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-20 18:32:13 +0530

    Added InvalidTeamException and InvalidTournamentException

---
+++ src/main/java/com/gb/cricscore/exception/InvalidTeamException.java
@@ -0,0 +1,7 @@
+package com.gb.cricscore.exception;
+
+public class InvalidTeamException extends Throwable {
+    public InvalidTeamException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/gb/cricscore/exception/InvalidTournamentException.java
@@ -0,0 +1,7 @@
+package com.gb.cricscore.exception;
+
+public class InvalidTournamentException extends Exception {
+    public InvalidTournamentException(String message) {
+        super(message);
+    }
+}
