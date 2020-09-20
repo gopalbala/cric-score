1f6c50a167178042093ac0b06118b7e2513e505f
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-21 00:09:34 +0530

    added getter and setter

--- src/main/java/com/gb/cricscore/model/match/Stadium.java
+++ src/main/java/com/gb/cricscore/model/match/Stadium.java
@@ -1,8 +1,12 @@
 package com.gb.cricscore.model.match;
 
 import com.gb.cricscore.common.Address;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Stadium {
-    String name;
-    Address address;
+    private String name;
+    private Address address;
 }
