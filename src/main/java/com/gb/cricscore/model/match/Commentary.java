c0e7d30c500c8f0306360be619efd50e78fa8e8d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-20 13:41:32 +0530

    Added Commentary

---
+++ src/main/java/com/gb/cricscore/model/match/Commentary.java
@@ -0,0 +1,11 @@
+package com.gb.cricscore.model.match;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Commentary {
+    private String commentator;
+    private String comment;
+}
