1d0fcb80dbc030602c7d5eb8e155838c2ac258f4
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-20 13:23:30 +0530

    Added Ball and BallType

---
+++ src/main/java/com/gb/cricscore/model/match/Ball.java
@@ -0,0 +1,14 @@
+package com.gb.cricscore.model.match;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Ball {
+    private int overNumber;
+    private BallType ballType;
+    private String bowledBy;
+    private String facedBy;
+    private int runsScored;
+}
---
+++ src/main/java/com/gb/cricscore/model/match/BallType.java
@@ -0,0 +1,8 @@
+package com.gb.cricscore.model.match;
+
+public enum BallType {
+    NORMAL,
+    WIDE,
+    NO_BALL,
+    WICKET
+}
