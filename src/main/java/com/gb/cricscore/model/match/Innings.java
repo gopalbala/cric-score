48d4d3dd6de3c177fcd961b28ed1741255e87480
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-20 13:41:45 +0530

    Added Innings

---
+++ src/main/java/com/gb/cricscore/model/match/Innings.java
@@ -0,0 +1,12 @@
+package com.gb.cricscore.model.match;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Innings {
+    List<Over> overs;
+}
