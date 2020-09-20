9ebaf6569d376a76b68e8f3cc4ba016665916802
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-21 00:08:49 +0530

    added additional functionalities

--- src/main/java/com/gb/cricscore/model/people/Admin.java
+++ src/main/java/com/gb/cricscore/model/people/Admin.java
@@ -1,5 +1,6 @@
 package com.gb.cricscore.model.people;
 
+import com.gb.cricscore.exception.InvalidMatch;
 import com.gb.cricscore.exception.InvalidTeamException;
 import com.gb.cricscore.exception.InvalidTournamentException;
 import com.gb.cricscore.model.match.Fixture;
@@ -30,30 +31,38 @@ public class Admin extends Person {
         DataSink.teamMap.putIfAbsent(team.getName(), team);
     }
 
-    public void addPlayer(Player player) throws InvalidTeamException, InvalidTournamentException {
-        if (DataSink.tournamentMap.get(player.getTournament()) == null)
+    public void addPlayer(Player player, String team, String tour) throws InvalidTeamException, InvalidTournamentException {
+        if (DataSink.tournamentMap.get(team) == null)
             throw new InvalidTournamentException("Invalid tournament");
-        if (DataSink.teamMap.get(player.getTeam()) == null)
+        if (DataSink.teamMap.get(tour) == null)
             throw new InvalidTeamException("Invalid team");
-        DataSink.teamMap.get(player.getTeam())
+        DataSink.teamMap.get(team)
                 .getPlayers().add(player);
 
     }
 
-    public void addUmpire(Umpire umpire) {
-
+    public void addUmpire(Umpire umpire, String match) throws InvalidMatch {
+        if (DataSink.matchMap.get(match) == null)
+            throw new InvalidMatch("Invalid match");
+        DataSink.matchMap.get(match).getUmpires().add(umpire);
     }
 
-    public void addCommentator(Commentator commentator) {
-
+    public void addCommentator(Commentator commentator, String match) throws InvalidMatch {
+        if (DataSink.matchMap.get(match) == null)
+            throw new InvalidMatch("Invalid match");
+        DataSink.matchMap.get(match).getCommentators().add(commentator);
+        DataSink.commentators.putIfAbsent(commentator.getName(), commentator);
     }
 
-    public void addScorer(Scorer scorer) {
-
+    public void addScorer(Scorer scorer, String match) throws InvalidMatch {
+        if (DataSink.matchMap.get(match) == null)
+            throw new InvalidMatch("Invalid match");
+        DataSink.matchMap.get(match).getScorers().add(scorer);
+        DataSink.scorerMap.putIfAbsent(scorer.getName(), scorer);
     }
 
     public void addStadium(Stadium stadium) {
-
+        DataSink.stadiumMap.putIfAbsent(stadium.getName(), stadium);
     }
 
     public void addFixture(Fixture fixture) {
