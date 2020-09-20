65eefb3320381caa76b35799c0eaf88297cccbc8
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-21 00:18:48 +0530

    updated DataSink

--- src/main/java/com/gb/cricscore/repository/DataSink.java
+++ src/main/java/com/gb/cricscore/repository/DataSink.java
@@ -1,7 +1,10 @@
 package com.gb.cricscore.repository;
 
 import com.gb.cricscore.model.match.Match;
+import com.gb.cricscore.model.match.Stadium;
 import com.gb.cricscore.model.match.Tournament;
+import com.gb.cricscore.model.people.Commentator;
+import com.gb.cricscore.model.people.Scorer;
 import com.gb.cricscore.model.people.Team;
 
 import java.util.HashMap;
@@ -11,4 +14,7 @@ public class DataSink {
     public static Map<String, Tournament> tournamentMap = new HashMap<>();
     public static Map<String, Match> matchMap = new HashMap<>();
     public static Map<String, Team> teamMap = new HashMap<>();
+    public static Map<String, Commentator> commentators = new HashMap<>();
+    public static Map<String, Scorer> scorerMap = new HashMap<>();
+    public static Map<String, Stadium> stadiumMap = new HashMap<>();
 }
