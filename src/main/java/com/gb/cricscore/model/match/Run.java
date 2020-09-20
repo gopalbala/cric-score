64c8519f6e93dae1823182047bc53fe8167728ae
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-20 13:24:05 +0530

    Added Run and RunType

---
+++ src/main/java/com/gb/cricscore/model/match/Run.java
@@ -0,0 +1,11 @@
+package com.gb.cricscore.model.match;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Run {
+    private int scoredRun;
+    private RunType runType;
+}
---
+++ src/main/java/com/gb/cricscore/model/match/RunType.java
@@ -0,0 +1,14 @@
+package com.gb.cricscore.model.match;
+
+public enum RunType {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FOUR_WIDE,
+    SIX,
+    NO_BALL_SIX,
+    BYE,
+    LEG_BYE,
+    OVERTHROW
+}
