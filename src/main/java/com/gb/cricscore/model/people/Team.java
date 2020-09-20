1ae051fd4c5df1e3cb3ff222380cbed088ccd204
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-20 18:41:53 +0530

    updated team

--- src/main/java/com/gb/cricscore/model/people/Team.java
+++ src/main/java/com/gb/cricscore/model/people/Team.java
@@ -3,18 +3,24 @@ package com.gb.cricscore.model.people;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 public class Team {
     private final String name;
-    private List<Player> players;
-    private List<Coach> coaches;
-    private List<Physicio> physicios;
+    private final String tourName;
+    private Set<Player> players;
+    private Set<Coach> coaches;
+    private Set<Physicio> physicios;
     private Manager manager;
 
-    public Team(String name) {
+    public Team(String name, String tourName) {
         this.name = name;
+        this.tourName = tourName;
+        players = new HashSet<>();
+        coaches = new HashSet<>();
+        physicios = new HashSet<>();
     }
 }
