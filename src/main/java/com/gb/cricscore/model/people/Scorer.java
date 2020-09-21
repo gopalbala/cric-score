c4bc036c15f1d65e4946e7ae7cbfbee2d6487fb5
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-22 00:27:36 +0530

    minor update

--- src/main/java/com/gb/cricscore/model/people/Scorer.java
+++ src/main/java/com/gb/cricscore/model/people/Scorer.java
@@ -1,7 +1,13 @@
 package com.gb.cricscore.model.people;
 
+import com.gb.cricscore.model.match.Ball;
+
 public class Scorer extends Person {
     public Scorer(String name) {
         super(name);
+    }
+
+    public void setScore(Ball ball) {
+        ball.getRunsScored();
     }
 }
