24228fe77788aab1cd92e2618369c8155bf59a94
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-22 00:17:53 +0530

    Added Fall

---
+++ src/main/java/com/gb/cricscore/model/match/Fall.java
@@ -0,0 +1,13 @@
+package com.gb.cricscore.model.match;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Fall {
+    private int fallNo;
+    private String player;
+    private int scoredRuns;
+
+}
