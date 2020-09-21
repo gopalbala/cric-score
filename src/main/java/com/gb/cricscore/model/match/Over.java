26fde31a5123bdb59fefc4febd0701f91eb1f470
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-22 00:17:44 +0530

    Added Over

--- src/main/java/com/gb/cricscore/model/match/Over.java
+++ src/main/java/com/gb/cricscore/model/match/Over.java
@@ -7,7 +7,36 @@ import java.util.List;
 
 @Getter
 @Setter
+
 public class Over {
     private List<Ball> balls;
     private List<Run> runs;
+
+    private boolean isMaidenOver() {
+        for (Ball ball : balls) {
+            if (ball.getRunsScored() > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getWicketsInOver() {
+        int wkCount = 0;
+        for (Ball ball : balls) {
+            if (ball.getWicket() != null) {
+                wkCount += 1;
+            }
+        }
+        return wkCount;
+    }
+
+    private int getRunsScoredInOver() {
+        int runsScored = 0;
+        for (Ball ball : balls) {
+            runsScored += ball.getRunsScored();
+        }
+        return runsScored;
+    }
+
 }
