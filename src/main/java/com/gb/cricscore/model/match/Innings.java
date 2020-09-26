8cdb4657003048ab5cdc4de4b2fa231b568ad79f
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-27 02:19:26 +0530

    initialized overs for Innings

--- src/main/java/com/gb/cricscore/model/match/Innings.java
+++ src/main/java/com/gb/cricscore/model/match/Innings.java
@@ -3,10 +3,15 @@ package com.gb.cricscore.model.match;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Innings {
     List<Over> overs;
+
+    public Innings() {
+        overs = new ArrayList<>();
+    }
 }
