9980d28d67f8edd51a9d96e83da3d83db81c9763
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-09-28 00:32:24 +0530

    called update ScoreCard

--- src/main/java/com/gb/cricscore/model/people/Scorer.java
+++ src/main/java/com/gb/cricscore/model/people/Scorer.java
@@ -1,13 +1,16 @@
 package com.gb.cricscore.model.people;
 
+import com.gb.cricscore.exception.InvalidMatch;
 import com.gb.cricscore.model.match.Ball;
+import com.gb.cricscore.model.match.ScoreCard;
 
 public class Scorer extends Person {
     public Scorer(String name) {
         super(name);
     }
 
-    public void setScore(Ball ball) {
-        ball.getRunsScored();
+    public void setScore(Ball ball, String match, int innings) throws InvalidMatch {
+        ScoreCard scoreCard = ScoreCard.INSTANCE(match, innings);
+        scoreCard.setScoreCardForBall(ball);
     }
 }
